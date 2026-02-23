3335c288626dc5053ddcf314bb3f2f7a2b06d673
khizarkhan07 <khizar062@gmail.com>
2026-02-23 19:31:03 +0500

    Setup Husky pre-commit with smart-code-reviewer npm package

--- npm-package/test-hook.ts
+++
@@ -1,8 +0,0 @@
-// Quick test file to trigger pre-commit hook
-function getData(x: any) {
-  var result = fetch("/api/" + x);
-  var d = result;
-  console.log(d);
-  return d;
-}
-export default getData;
